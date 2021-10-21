a72d4f4213502e50ce03d4e4d24d39ebe12b2a54
urd00m <alan.linghao.wang@gmail.com>
2021-10-21 18:19:06 -0500

    medium

---
+++ LeetCode/medium/uniquePathsWithObstacles.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        long m = obstacleGrid.size(); 
+        long n = obstacleGrid[0].size(); 
+        vector<vector<int>> dp(m, vector(n, 0)); 
+        dp[0][0] = 1; 
+        if(obstacleGrid[0][0] == 1) return 0; 
+        
+        //set up the unique paths
+        for(long i = 0; i < n; i++) { //columns
+            dp[0][i] = 1; 
+            if(obstacleGrid[0][i] == 1) {
+                dp[0][i] = 0; 
+                break; 
+            } 
+        }
+        for(long i = 0; i < m; i++) { //rows
+            dp[i][0] = 1; 
+            if(obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0; 
+                break;
+            }
+        }
+        
+        //dp 
+        for(long i = 1; i < m; i++) {
+            for(long j = 1; j < n; j++) {
+                if(obstacleGrid[i][j] == 1) continue;
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
+            }
+        }
+        
+        return dp[m-1][n-1]; 
+    }
+};
