af42598aef2b6c08167f913ac6e5b946153199d1
urd00m <alan.linghao.wang@gmail.com>
2022-04-15 10:55:29 -0500

    Daily'

---
+++ LeetCode/medium/trimBST.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return root;
+        if (root.val > high) return trimBST(root.left, low, high);
+        if (root.val < low) return trimBST(root.right, low, high);
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;       
+    }
+}
+
+// Solution given by problem, didn't have time to write out c++ solution
