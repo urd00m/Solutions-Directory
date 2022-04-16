c176020ab06f8c8ff5f320888159b692b1f2f86d
urd00m <alan.linghao.wang@gmail.com>
2022-04-16 11:23:34 -0500

    Daily

---
+++ LeetCode/medium/convertBST.java
@@ -0,0 +1,15 @@
+class Solution {
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
+
+// Answer copied from solution again, no time to figure it out 
