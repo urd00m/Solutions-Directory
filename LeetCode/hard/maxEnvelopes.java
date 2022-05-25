8f920202beaa6208bb1a4fc7403dade6c9f0ccdb
urd00m <alan.linghao.wang@gmail.com>
2022-05-25 16:17:32 -0500

    Daily but no time to implement

---
+++ LeetCode/hard/maxEnvelopes.java
@@ -0,0 +1,4 @@
+// Would write this in java since I can customize the sorts 
+
+// LIS problem, just sort by height if equal sort by width
+// that simplifies the problem where we don't care about height
