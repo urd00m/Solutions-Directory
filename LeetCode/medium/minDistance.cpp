9d24cd6a422a8d952a4bae9f32485a781f721dec
urd00m <alan.linghao.wang@gmail.com>
2022-06-14 14:27:25 -0500

    Added in my lcs function that is more optimized in a way

--- LeetCode/medium/minDistance.cpp
+++ LeetCode/medium/minDistance.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp = vector(word1.size()+1, vector(word2.size()+1, -1)); 
-        
-        return word1.size()+word2.size()-2*lcs(word1, word2, word1.size(), word2.size(), dp); 
+        vector<vector<int>> dp = vector(word1.size(), vector(word2.size(), -1)); 
+    
+        return word1.size()+word2.size()-2*lcs(word1, word2, 0, 0, dp); 
     }
-
+/*
     int lcs(string& X, string& Y, int m, int n, vector<vector<int> >& dp) {
         if (m == 0 || n == 0)
             return 0;
@@ -15,6 +15,20 @@ public:
         if (dp[m][n] != -1) {
             return dp[m][n];
         }
-        return dp[m][n] = max(lcs(X, Y, m, n - 1, dp),                          lcs(X, Y, m - 1, n, dp));
+        return dp[m][n] = max(lcs(X, Y, m, n - 1, dp), lcs(X, Y, m - 1, n, dp));
+    }
+*/
+    int lcs(string& a, string& b, int i, int j, vector<vector<int> >& dp) {
+        if(i == a.size() || j == b.size()) return 0; 
+        
+        // memoization
+        if(dp[i][j] != -1) return dp[i][j]; 
+        
+        // equal
+        if(a[i] == b[j]) 
+            return dp[i][j] = 1 + lcs(a, b, i+1, j+1, dp);
+        else 
+            return dp[i][j] = max(lcs(a, b, i+1, j, dp), lcs(a, b, i, j+1, dp)); 
+        
     }
 };
