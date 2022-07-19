4ef32a67590ba3f318258882c9a626f90e75f992
urd00m <alan.linghao.wang@gmail.com>
2022-07-19 14:09:04 -0500

    daily dp problems

--- LeetCode/medium/numTrees.cpp
+++ LeetCode/medium/numTrees.cpp
@@ -14,3 +14,23 @@ public:
         return build[n]; 
     }
 };
+
+// recursive solution
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<vector<int>> dp = vector(n, vector(n, -1)); 
+        return recur(dp, 0, n-1); 
+    }
+    
+    static int recur(vector<vector<int>>& dp, int l, int r) {
+        if(r <= l) return 1; 
+        if(dp[l][r] != -1) return dp[l][r]; 
+        
+        int ret = 0;
+        for(int i = l; i <= r; i++) {
+            ret += recur(dp, l, i-1)*recur(dp, i+1,r);
+        }
+        return dp[l][r] = ret; 
+    }
+};
