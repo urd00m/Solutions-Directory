27231f1c76533d71cb5befa0bddee6c76cd09464
urd00m <alan.linghao.wang@gmail.com>
2022-09-08 08:47:51 -0500

    daily

---
+++ LeetCode/easy/inorderTraversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ret; inorder(ret, root);
+        return ret; 
+    }
+    
+    void inorder(vector<int>& ret, TreeNode* cur) {
+        if(cur == nullptr) return; 
+        
+        inorder(ret, cur->left);
+        ret.push_back(cur->val); 
+        inorder(ret, cur->right);
+    }
+};
