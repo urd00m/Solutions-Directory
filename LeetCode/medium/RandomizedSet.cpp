c1ddfe16e4082a7adbbcae19033297c7d673e186
urd00m <alan.linghao.wang@gmail.com>
2022-11-29 08:39:11 -0600

    daily

--- LeetCode/medium/RandomizedSet.cpp
+++ LeetCode/medium/RandomizedSet.cpp
@@ -33,3 +33,43 @@ private:
     unordered_map<int, int> m;
 };
 
+
+/*
+class RandomizedSet {
+public:
+    unordered_map<int, int> m;
+    vector<int> e;  
+    RandomizedSet() {
+        m.clear(); e.clear(); 
+    }
+    
+    bool insert(int val) {
+        if(m.find(val) != m.end()) return false; 
+        m[val] = e.size(); 
+        e.push_back(val);
+        return true; 
+    }
+    
+    bool remove(int val) {
+        if(m.find(val) == m.end()) return false;
+        e[m[val]] = e[e.size()-1]; 
+        m[e[e.size()-1]] = m[val];
+        e.pop_back(); 
+        m.erase(val); 
+        return true; 
+    }
+    
+    int getRandom() {
+        return e[rand() % e.size()]; 
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+
+*/
