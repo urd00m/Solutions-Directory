f16ea40cc7090724e7b18ad046e841c25e585e26
urd00m <alan.linghao.wang@gmail.com>
2023-08-28 10:20:34 -0500

    daily

--- LeetCode/easy/MyStack.cpp
+++ LeetCode/easy/MyStack.cpp
@@ -1,5 +1,44 @@
 class MyStack {
 public:
+    queue<int> q; 
+
+    MyStack() {
+        q = queue<int>(); 
+    }
+    
+    void push(int x) {
+        int s = q.size(); 
+        q.push(x);
+        for(int i = 0; i < s; i++) {
+            q.push(q.front()); 
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int ret = q.front();
+        q.pop(); 
+        return ret;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty(); 
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */class MyStack {
+public:
     vector<int> mystack; 
     MyStack() {
         mystack = vector<int>(0); 
